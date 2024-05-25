89ead02ee72e181c8ca98a02aff23caccaed4c06
leVert <selmamathis15@gmail.com>
2024-05-25 09:10:51 +0300

    initial setup of file and dependancies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
+
  const Home=()=>{
+    const loggedIn= { firstName: 'Joshua'};
     return (
-        <div>Home</div>
+        <section className='home'>
+            <div className='home-content'>
+                <header className="homeheader">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+            </div>
+        </section>
     )
  }
 
