7db2a30958eb3dd0fc45febdfc8f60d59e764904
leVert <selmamathis15@gmail.com>
2024-06-12 18:58:03 +0300

    fixed typos

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -7,15 +7,18 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 const DoughnutChart = ({accounts}: DoughnutChartProps) => {
+  const accountNames=accounts.map((a)=>a.name);
+  const balances=accounts.map((a)=>a.currentBalance);
+
     const data={
         datasets: [
             {
                 label: 'Banks',
-                data: [1250, 2500, 3750], //three banks for this case
+                data: balances, //three banks for this case
                 backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'] //belongs to javascript styling
             }
         ],
-        labels: ['Bank 1', 'Bank 2', 'Bank 3']
+        labels: accountNames
     }
   return (
     <Doughnut data={data}/>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -15,13 +15,38 @@ const {
     APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
 }=process.env;
 
+export const getUserInfo= async({userId}:getUserInfoProps)=>{
+    try{
+        const {database} =await createAdminClient();
+
+        const user = await database.listDocuments(
+            DATABASE_ID!,
+            USER_COLLECTION_ID!,
+            [Query.equal('userId', [userId])]
+        )
+
+        return parseStringify(user.documents[0]);
+    }catch(error){
+        console.log(error)
+    }
+}
 //try catch for async functions
 export const signIn=async({email, password}: signInProps)=>{
     try {
         //Mutation/Database/make fetch
         const { account } = await createAdminClient();
-        const response=await account.createEmailPasswordSession(email, password); 
-        return parseStringify(response);
+        const session=await account.createEmailPasswordSession(email, password); 
+        
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+          });
+
+          const user= await getUserInfo({userId: session.userId})
+
+        return parseStringify(user);
     } catch (error) {
         console.error('Error', error);
     }
@@ -82,7 +107,8 @@ export const signUp=async({password, ...userData}: SignUpParams)=>{
 export async function getLoggedInUser() {
     try {
       const { account } = await createSessionClient();
-      const user=await account.get();
+      const result=await account.get();
+      const user=await getUserInfo({userId: result.$id})
       return parseStringify(user);
     } catch (error) {
       return null;
@@ -210,3 +236,36 @@ export const exchangePublicToken=async({
     }
 }
    
+export const getBanks = async ({userId}:getBanksProps)=>{
+    try{
+        const {database} =await createAdminClient();
+
+        const banks = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal('userId', [userId])]
+        )
+
+        return parseStringify(banks.documents);
+    }catch(error){
+        console.log(error)
+    }
+}
+    
+
+export const getBank = async ({documentId}:getBankProps)=>{
+    try{
+        const {database} =await createAdminClient();
+
+        const bank = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal('$id', [documentId])]
+        )
+
+        return parseStringify(bank.documents[0]);
+    }catch(error){
+        console.log(error)
+    }
+}
+ 
